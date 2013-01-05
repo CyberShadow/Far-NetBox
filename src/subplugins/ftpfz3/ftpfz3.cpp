eb0a4fd79ee52f508a6f00e743e76fce49fc659b
Michael Lukashov <michael.lukashov@gmail.com>
2013-01-05 18:15:50 +0300

    Remove subplugin_startup_info_t startup_info; (already done in TBaseSubplugin)

--- src/subplugins/ftpfz3/ftpfz3.cpp
+++ src/subplugins/ftpfz3/ftpfz3.cpp
@@ -57,13 +57,13 @@ BOOL WINAPI DllMain(HINSTANCE HInstance, DWORD Reason, LPVOID /*ptr*/ )
 struct gdisk_ctx_t
 {
   TSubplugin * Subplugin;
-  subplugin_startup_info_t startup_info;
+  // subplugin_startup_info_t startup_info;
 };
 //------------------------------------------------------------------------------
 
 static const subplugin_version_t * get_min_netbox_version()
 {
-  SUBPLUGIN_VERSION_BODY(2, 1, 13, 166);
+  SUBPLUGIN_VERSION_BODY(2, 1, 20, 200);
 }
 
 static const subplugin_version_t * get_subplugin_version()
@@ -116,9 +116,9 @@ struct subplugin_impl_t
     assert(subplugin);
     gdisk_ctx_t * ctx = static_cast<gdisk_ctx_t *>(startup_info->NSF->pcalloc(subplugin, sizeof(*ctx)));
     assert(ctx);
-    memset(&ctx->startup_info, 0, sizeof(ctx->startup_info));
-    memmove(&ctx->startup_info, startup_info, startup_info->struct_size >= sizeof(ctx->startup_info) ?
-      sizeof(ctx->startup_info) : startup_info->struct_size);
+    // memset(&ctx->startup_info, 0, sizeof(ctx->startup_info));
+    // memmove(&ctx->startup_info, startup_info, startup_info->struct_size >= sizeof(ctx->startup_info) ?
+      // sizeof(ctx->startup_info) : startup_info->struct_size);
 
     subplugin->vtable = &vtable;
     subplugin->impl_ctx = ctx;
