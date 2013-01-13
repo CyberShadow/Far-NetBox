d33bcd16011c4142bce8e1d7fd0d0a745fb11ac3
Michael Lukashov <michael.lukashov@gmail.com>
2013-01-13 22:51:43 +0300

    UnityBuildMain

--- src/subplugins/Ftp/UnityBuildMain.cpp
+++ src/subplugins/Ftp/UnityBuildMain.cpp
@@ -6,18 +6,45 @@
 #include <Sysutils.cpp>
 #include <UnicodeString.cpp>
 #include <local.cpp>
+
 #include <Common.cpp>
+#include <RemoteFiles.cpp>
+#include <Terminal.cpp>
 #include <Exceptions.cpp>
-#include <FileBuffer.cpp>
-#include <FtpFileSystem.cpp>
-#include <SessionData.cpp>
 #include <FileOperationProgress.cpp>
+#include <Queue.cpp>
+#include <SecureShell.cpp>
+#include <SessionInfo.cpp>
+#include <CoreMain.cpp>
+#include <FileMasks.cpp>
+#include <CopyParam.cpp>
+#include <SessionData.cpp>
+#include <Configuration.cpp>
+#include <ScpFileSystem.cpp>
+#include <FtpFileSystem.cpp>
+#include <FileSystemProxy.cpp>
+#include <PuttyIntf.cpp>
+#include <Cryptography.cpp>
+#include <FileBuffer.cpp>
+#include <NamedObjs.cpp>
+#include <HierarchicalStorage.cpp>
+#include <Option.cpp>
+#include <FileInfo.cpp>
+#include <FileSystems.cpp>
+#include <Bookmarks.cpp>
+#include <WinSCPSecurity.cpp>
+#include <SynchronizeController.cpp>
+#include <GUIConfiguration.cpp>
+#include <GUITools.cpp>
+
 // TODO: Remove dependence from FAR and NetBox
 #include <FarPlugin.cpp>
 #include <FarDialog.cpp>
 #include <NetBoxSubPlugin.cpp>
+#include <SubpluginsManager.cpp>
 
 #include "Main.cpp"
 #include "Subplugin.cpp"
+// #include "FtpFileSystem.cpp"
 
 #pragma warning(pop)
