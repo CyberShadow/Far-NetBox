045b04a3aa8805c715af065e4be9b392a7424105
Michael Lukashov <michael.lukashov@gmail.com>
2013-01-15 23:59:21 +0300

    UnityBuildMain.cpp

--- src/subplugins/Ftp/UnityBuildMain.cpp
+++ src/subplugins/Ftp/UnityBuildMain.cpp
@@ -2,49 +2,57 @@
 #pragma warning(disable: 4512) // assignment operator could not be generated
 #pragma warning(disable: 4068) // unknown pragma
 
+#define PUTTY_DO_GLOBALS
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <Classes.cpp>
 #include <Sysutils.cpp>
 #include <UnicodeString.cpp>
 #include <local.cpp>
 
 #include <Common.cpp>
-#include <RemoteFiles.cpp>
-#include <Terminal.cpp>
-#include <Exceptions.cpp>
+// #include <RemoteFiles.cpp>
+// #include <Terminal.cpp>
+// #include <Exceptions.cpp>
 #include <FileOperationProgress.cpp>
-#include <Queue.cpp>
-#include <SecureShell.cpp>
+// #include <Queue.cpp>
+// #include <SecureShell.cpp>
 #include <SessionInfo.cpp>
 #include <CoreMain.cpp>
 #include <FileMasks.cpp>
 #include <CopyParam.cpp>
 #include <SessionData.cpp>
 #include <Configuration.cpp>
-#include <ScpFileSystem.cpp>
+// #include <ScpFileSystem.cpp>
 #include <FtpFileSystem.cpp>
-#include <FileSystemProxy.cpp>
-#include <PuttyIntf.cpp>
-#include <Cryptography.cpp>
+// #include <PuttyIntf.cpp>
+// #include <Cryptography.cpp>
 #include <FileBuffer.cpp>
 #include <NamedObjs.cpp>
-#include <HierarchicalStorage.cpp>
-#include <Option.cpp>
+// #include <HierarchicalStorage.cpp>
+// #include <Option.cpp>
 #include <FileInfo.cpp>
 #include <FileSystems.cpp>
-#include <Bookmarks.cpp>
-#include <WinSCPSecurity.cpp>
+// #include <Bookmarks.cpp>
+// #include <WinSCPSecurity.cpp>
 #include <SynchronizeController.cpp>
-#include <GUIConfiguration.cpp>
-#include <GUITools.cpp>
+// #include <GUIConfiguration.cpp>
+// #include <GUITools.cpp>
+#include <FileSystemProxy.cpp>
 
 // TODO: Remove dependence from FAR and NetBox
-#include <FarPlugin.cpp>
-#include <FarDialog.cpp>
+// #include <FarPlugin.cpp>
+// #include <FarDialog.cpp>
+#include <FarUtil.cpp>
+// #include <WinSCPFileSystem.cpp>
+// #include <WinSCPPlugin.cpp>
 #include <NetBoxSubPlugin.cpp>
-#include <SubpluginsManager.cpp>
+// #include <FarConfiguration.cpp>
+// #include <FarInterface.cpp>
+// #include <XmlStorage.cpp>
+// #include <SubpluginsManager.cpp>
 
 #include "Main.cpp"
 #include "Subplugin.cpp"
-// #include "FtpFileSystem.cpp"
 
 #pragma warning(pop)
