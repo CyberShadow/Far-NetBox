837164c9315ba996a29712a46a2523dc4e5b0cb2
Michael Lukashov <michael.lukashov@gmail.com>
2013-01-17 23:24:02 +0300

    Ftp/UnityBuildCore.cpp

---
+++ src/subplugins/Ftp/UnityBuildCore.cpp
@@ -0,0 +1,45 @@
+#define PUTTY_DO_GLOBALS
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma warning(push)
+#pragma warning(disable: 4068) // unknown pragma
+#pragma warning(disable: 4100) // unreferenced formal parameter
+
+#include "../../base/Classes.cpp"
+#include "../../base/SysUtils.cpp"
+#include "../../base/UnicodeString.cpp"
+#include "../../base/local.cpp"
+
+#include "../../core/Common.cpp"
+#include "../../core/RemoteFiles.cpp"
+#include "../../core/Terminal.cpp"
+#include "../../core/Exceptions.cpp"
+#include "../../core/FileOperationProgress.cpp"
+#include "../../core/Queue.cpp"
+#include "../../core/SecureShell.cpp"
+#include "../../core/SessionInfo.cpp"
+#include "../../core/CoreMain.cpp"
+#include "../../core/FileMasks.cpp"
+#include "../../core/CopyParam.cpp"
+#include "../../core/SessionData.cpp"
+#include "../../core/Configuration.cpp"
+// #include "../../core/ScpFileSystem.cpp"
+#include "../../core/FtpFileSystem.cpp"
+// #include "../../core/SftpFileSystem.cpp"
+// #include "../../core/WebDAVFileSystem.cpp"
+#include "../../core/FileSystemProxy.cpp"
+#include "../../core/PuttyIntf.cpp"
+#include "../../core/Cryptography.cpp"
+#include "../../core/FileBuffer.cpp"
+#include "../../core/NamedObjs.cpp"
+#include "../../core/HierarchicalStorage.cpp"
+#include "../../core/Option.cpp"
+#include "../../core/FileInfo.cpp"
+#include "../../core/FileSystems.cpp"
+#include "../../core/Bookmarks.cpp"
+#include "../../core/WinSCPSecurity.cpp"
+#include "../../windows/SynchronizeController.cpp"
+#include "../../windows/GUIConfiguration.cpp"
+#include "../../windows/GUITools.cpp"
+
+#pragma warning(pop)
--- src/subplugins/Ftp/UnityBuildMain.cpp
+++ src/subplugins/Ftp/UnityBuildMain.cpp
@@ -2,49 +2,48 @@
 #pragma warning(disable: 4512) // assignment operator could not be generated
 #pragma warning(disable: 4068) // unknown pragma
 
-// #define PUTTY_DO_GLOBALS
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <Classes.cpp>
-#include <Sysutils.cpp>
-#include <UnicodeString.cpp>
-#include <local.cpp>
+// #include <Classes.cpp>
+// #include <Sysutils.cpp>
+// #include <UnicodeString.cpp>
+// #include <local.cpp>
 
-#include <Common.cpp>
-#include <RemoteFiles.cpp>
+// #include <Common.cpp>
+// #include <RemoteFiles.cpp>
 // #include <Terminal.cpp>
-#include <Exceptions.cpp>
-#include <FileOperationProgress.cpp>
+// #include <Exceptions.cpp>
+// #include <FileOperationProgress.cpp>
 // #include <Queue.cpp>
 // #include <SecureShell.cpp>
-#include <SessionInfo.cpp>
-#include <CoreMain.cpp>
-#include <FileMasks.cpp>
-#include <CopyParam.cpp>
-#include <SessionData.cpp>
-#include <Configuration.cpp>
+// #include <SessionInfo.cpp>
+// #include <CoreMain.cpp>
+// #include <FileMasks.cpp>
+// #include <CopyParam.cpp>
+// #include <SessionData.cpp>
+// #include <Configuration.cpp>
 // #include <ScpFileSystem.cpp>
 // #include <PuttyIntf.cpp>
 // #include <Cryptography.cpp>
-#include <FileBuffer.cpp>
-#include <NamedObjs.cpp>
+// #include <FileBuffer.cpp>
+// #include <NamedObjs.cpp>
 // #include <HierarchicalStorage.cpp>
 // #include <Option.cpp>
-#include <FileInfo.cpp>
-#include <FileSystems.cpp>
+// #include <FileInfo.cpp>
+// #include <FileSystems.cpp>
 // #include <Bookmarks.cpp>
 // #include <WinSCPSecurity.cpp>
-#include <SynchronizeController.cpp>
+// #include <SynchronizeController.cpp>
 // #include <GUIConfiguration.cpp>
 // #include <GUITools.cpp>
-#include <FileSystemProxy.cpp>
+// #include <FileSystemProxy.cpp>
 
-#include <FtpFileSystem.cpp>
+// #include <FtpFileSystem.cpp>
 
 // TODO: Remove dependence from FAR and NetBox
 // #include <FarPlugin.cpp>
 // #include <FarDialog.cpp>
-#include <FarUtil.cpp>
+// #include <FarUtil.cpp>
 // #include <WinSCPFileSystem.cpp>
 // #include <WinSCPPlugin.cpp>
 #include <NetBoxSubPlugin.cpp>
